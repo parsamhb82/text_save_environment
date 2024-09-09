148f23400f2438a05f7ea3aaca1ef24276ec9e9d
parsamhb82 <parsa1382mohebian@gmail.com>
2024-09-09 20:38:23 +0330

    models initialized

---
+++ txt_environmet/file/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class File(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True, null=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
